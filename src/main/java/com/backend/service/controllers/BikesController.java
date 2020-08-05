cf679e1d8b3f2b3df30cc151652edade6505204e
monju <monjurmorshed793@gmail.com>
2020-08-06 00:59:32 +0600

    hard coded codes refactored with repository

--- src/main/java/com/backend/service/controllers/BikesController.java
+++ src/main/java/com/backend/service/controllers/BikesController.java
@@ -1,6 +1,8 @@
 package com.backend.service.controllers;
 
 import com.backend.service.models.Bike;
+import com.backend.service.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,20 +13,26 @@ import java.util.List;
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 
+    private final  BikeRepository bikeRepository;
+
+    public BikesController(BikeRepository bikeRepository) {
+        this.bikeRepository = bikeRepository;
+    }
+
     @GetMapping
     public List<Bike> list(){
-        List<Bike> bikes = new ArrayList<>();
-        return bikes;
+        return bikeRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike){
-
+        bikeRepository.save(bike);
     }
 
     @GetMapping("/{id}")
-    public Bike get(@PathVariable("id") long id){
-        return new Bike();
+    public Bike get(@PathVariable("id") long id)
+    {
+        return bikeRepository.getOne(id);
     }
 }
