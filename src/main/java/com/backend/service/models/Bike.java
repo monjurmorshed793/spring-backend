eecb7c2114bcb653b3fa9dd7eff28d6cd59ed228
monju <monjurmorshed793@gmail.com>
2020-08-06 01:02:35 +0600

    hibernate properties ignored in model. Date format changed.

--- src/main/java/com/backend/service/models/Bike.java
+++ src/main/java/com/backend/service/models/Bike.java
@@ -1,5 +1,9 @@
 package com.backend.service.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +13,7 @@ import java.sql.Date;
 import java.util.Objects;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Bike {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,6 +25,8 @@ public class Bike {
     private String model;
     private String serialNumber;
     private BigDecimal purchasePrice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date purchaseDate;
     private boolean contact;
 
